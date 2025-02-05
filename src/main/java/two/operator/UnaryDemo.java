e7691a50115ad916d677f8ce72f3b88af37c00e8
Dženan <dzenanhus04@gmail.com>
2025-02-05 17:26:10 +0100

    Hexadecimalno formiranje vrijednosti!!!

---
+++ src/main/java/two/operator/UnaryDemo.java
@@ -0,0 +1,17 @@
+package two.operator;
+
+public class UnaryDemo {
+    public static void main(String[] args) {
+        int number = 23;
+        number++;
+        System.out.println(number);
+        number = 23;
+        ++number;
+        System.out.println(number);
+        number = 23;
+        System.out.println(++number);
+        number = 23;
+        System.out.println(number++);
+        System.out.println(number);
+    }
+}
